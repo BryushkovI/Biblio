f52bf6f011dfab9aa02d177b985011f1825a7f1e
Илья <@DESKTOP-IT1SR69>
2025-02-10 19:20:59 +0300

    Добавьте файлы проекта.

---
+++ BiblioContext/Model/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace BiblioContext.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorFirstName {  get; set; }
+        public string AuthorLastName { get; set; }
+        public string Ganre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
